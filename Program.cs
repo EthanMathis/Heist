e75fd24641a6fe668a44a07af44735e99744dbaa
EthanMathis <ethan.mathis@outlook.com>
2021-06-01 10:06:11 -0500

    phase one complete

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+            Console.WriteLine("Plan Your Heist!");
+            Console.Write("Enter a name!");
+            string Name = Console.ReadLine();
+
+            TeamMember Person = new TeamMember(Name);
+
+            Person.GetSkill();
+            Person.GetCourage();
+
+            Console.WriteLine($"{Person.Name}'s skill level is {Person.SkillLevel} and their courage level is {Person.Courage}!");
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Heist
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+
+        public int SkillLevel { get; set; }
+
+        public double Courage { get; set; }
+
+
+        public TeamMember(string name)
+        {
+            Name = name;
+            SkillLevel = 0;
+            Courage = 0;
+        }
+
+        public void GetSkill()
+        {
+            while (true)
+            {
+                Console.Write("Enter a skill level!");
+                string SkillString = Console.ReadLine();
+                if (int.TryParse(SkillString, out int SkillInt))
+                {
+                    SkillLevel = SkillInt;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number greater than 0!");
+                }
+            }
+
+        }
+
+        public void GetCourage()
+        {
+            while (true)
+            {
+                Console.Write("Enter a courage level!");
+                string CourageString = Console.ReadLine();
+                if (double.TryParse(CourageString, out double CourageInt) && CourageInt > 0 && CourageInt < 2)
+                {
+                    Courage = CourageInt;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number greater than 0!");
+                }
+
+            }
+        }
+    }
+}
