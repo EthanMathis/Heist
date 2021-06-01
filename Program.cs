564c684daa5eddeaabda4c0778a87ae2b433066e
EthanMathis <ethan.mathis@outlook.com>
2021-06-01 09:26:46 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
