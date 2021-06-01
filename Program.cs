f72ffe3f9337db1806677a4afb73fc4d19fe71c8
dudley-codes <alexdudley@me.com>
2021-06-01 10:33:29 -0500

    completed phase2

--- Program.cs
+++ Program.cs
@@ -1,23 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Heist
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      Console.Clear();
+
+      List<TeamMember> HeistTeam = new List<TeamMember>();
+
+      Console.WriteLine("Plan Your Heist!");
+      while (true)
+      {
+        Console.Write("Enter a name! ");
+        string Name = Console.ReadLine();
+        if (Name.Length > 0)
         {
-            Console.Clear();
-
-            Console.WriteLine("Plan Your Heist!");
-            Console.Write("Enter a name!");
-            string Name = Console.ReadLine();
-
-            TeamMember Person = new TeamMember(Name);
-
-            Person.GetSkill();
-            Person.GetCourage();
-
-            Console.WriteLine($"{Person.Name}'s skill level is {Person.SkillLevel} and their courage level is {Person.Courage}!");
+          TeamMember Person = new TeamMember(Name);
+          Person.GetSkill();
+          Person.GetCourage();
+          HeistTeam.Add(Person);
         }
+        else break;
+      }
+
+      Console.WriteLine($"You have {HeistTeam.Count} members on your team");
+
+      foreach (TeamMember Person in HeistTeam)
+      {
+        Console.WriteLine($"{Person.Name}'s skill level is {Person.SkillLevel} and their courage level is {Person.Courage}!");
+      }
     }
+  }
 }
