3936ab816803c23d6fd62c0f467c8e9754c7a10c
Logan Demmy <frank.demmy@gmail.com>
2021-06-01 14:38:48 -0400

    ascii art

--- Program.cs
+++ Program.cs
@@ -10,24 +10,52 @@ namespace Heist
             Console.Clear();
 
             List<TeamMember> HeistTeam = new List<TeamMember>();
+            void header()
+            {
+                Console.WriteLine(@"
+ /$$   /$$           /$$             /$$            /$$$$$$                                   
+| $$  | $$          |__/            | $$           /$$__  $$                                  
+| $$  | $$  /$$$$$$  /$$  /$$$$$$$ /$$$$$$        | $$  \__/  /$$$$$$  /$$$$$$/$$$$   /$$$$$$ 
+| $$$$$$$$ /$$__  $$| $$ /$$_____/|_  $$_/        | $$ /$$$$ |____  $$| $$_  $$_  $$ /$$__  $$
+| $$__  $$| $$$$$$$$| $$|  $$$$$$   | $$          | $$|_  $$  /$$$$$$$| $$ \ $$ \ $$| $$$$$$$$
+| $$  | $$| $$_____/| $$ \____  $$  | $$ /$$      | $$  \ $$ /$$__  $$| $$ | $$ | $$| $$_____/
+| $$  | $$|  $$$$$$$| $$ /$$$$$$$/  |  $$$$/      |  $$$$$$/|  $$$$$$$| $$ | $$ | $$|  $$$$$$$
+|__/  |__/ \_______/|__/|_______/    \___/         \______/  \_______/|__/ |__/ |__/ \_______/           
+            
+                                                                                           ");
+            }
+            header();
 
             Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine("");
+            Console.Write("How many members are on your crew? ");
+            int teammembers;
             while (true)
             {
+                string members = Console.ReadLine();
+                if (int.TryParse(members, out teammembers))
+                {
+                    break;
+                }
+                Console.WriteLine("How about an actual number?");
+
+            }
+            Console.Clear();
+            for (int k = 0; k < teammembers; k++)
+            {
+
+                header();
                 Console.Write("Enter a name! ");
                 string Name = Console.ReadLine();
-                if (Name.Length > 0)
-                {
-                    TeamMember Person = new TeamMember(Name);
-                    Person.GetSkill();
-                    Person.GetCourage();
-                    HeistTeam.Add(Person);
-                }
-                else break;
+                TeamMember Person = new TeamMember(Name);
+                Person.GetSkill();
+                Person.GetCourage();
+                HeistTeam.Add(Person);
+                Console.Clear();
             }
 
-            Console.WriteLine($"You have {HeistTeam.Count} members on your team");
-
+            // Console.WriteLine($"You have {HeistTeam.Count} members on your team");
+            header();
             Console.Write("How many times are you going to try your crazy plan? ");
             int runs = 0;
             while (true)
@@ -52,6 +80,7 @@ namespace Heist
                 Console.WriteLine("How about a reasonable number?");
             }
 
+
             int Wins = 0;
             int Losses = 0;
 
@@ -65,6 +94,9 @@ namespace Heist
                 int luck = i.Next(-10, 10);
                 Difficulty = Difficulty + luck;
 
+
+                Console.Clear();
+                header();
                 Console.Write($"Bank difficulty is {Difficulty}, ");
 
 
@@ -78,20 +110,69 @@ namespace Heist
                 Console.WriteLine($"Team Skillz {TeamSkillz}");
 
 
+
+
                 if (Difficulty < TeamSkillz)
                 {
                     Wins++;
                     Console.WriteLine("You da best! You got all the monies");
-                    Console.WriteLine("");
+                    Console.WriteLine(@"
+   ||====================================================================||
+   ||//$\\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//$\\||
+   ||(100)==================| FEDERAL RESERVE NOTE |================(100)||
+   ||\\$//        ~         '------========--------'                \\$//||
+   ||<< /        /$\              // ____ \\                         \ >>||
+   ||>>|  12    //L\\            // ///..) \\         L38036133B   12 |<<||
+   ||<<|        \\ //           || <||  >\  ||                        |>>||
+   ||>>|         \$/            ||  $$ --/  ||        One Hundred     |<<||
+||====================================================================||>||
+||//$\\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//$\\||<||
+||(100)==================| FEDERAL RESERVE NOTE |================(100)||>||
+||\\$//        ~         '------========--------'                \\$//||\||
+||<< /        /$\              // ____ \\                         \ >>||)||
+||>>|  12    //L\\            // ///..) \\         L38036133B   12 |<<||/||
+||<<|        \\ //           || <||  >\  ||                        |>>||=||
+||>>|         \$/            ||  $$ --/  ||        One Hundred     |<<||
+||<<|      L38036133B        *\\  |\_/  //* series                 |>>||
+||>>|  12                     *\\/___\_//*   1989                  |<<||
+||<<\      Treasurer     ______/Franklin\________     Secretary 12 />>||
+||//$\                 ~|UNITED STATES OF AMERICA|~               /$\\||
+||(100)===================  ONE HUNDRED DOLLARS =================(100)||
+||\\$//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\$//||
+||====================================================================||
+");
+                    System.Threading.Thread.Sleep(4000);
                 }
                 else
                 {
                     Losses++;
-                    Console.WriteLine("Do not pass go. Do not collect $200. Go directly to jail!");
+                    Console.WriteLine($@"
+  _________________________
+     ||   ||     ||   ||
+     ||   ||, , ,||   ||
+     ||  (||/|/(\||/  ||
+     ||  ||| _'_`|||  ||
+     ||   || o o ||   ||
+     ||  (||  - `||)  ||
+     ||   ||  =  ||   ||
+     ||   ||\___/||   ||
+     ||___||) , (||___||
+    /||---||-\_/-||---||\
+   / ||--_||_____||_--|| \
+  (_(||)-| S123-45 |-(||)_)
+|'''''''''''''''''''''''''''|
+|      Do not pass go,      |
+|    Do not collect $200    |
+|   go straight to jail!!   |
+ '''''''''''''''''''''''''''
+");
                     Console.WriteLine("");
+                    System.Threading.Thread.Sleep(4000);
                 }
 
             }
+            Console.Clear();
+            header();
             Console.WriteLine($"Your team was successfull {Wins} times & failed miserably {Losses} times.");
         }
     }
