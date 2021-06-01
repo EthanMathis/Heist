6c65ccb02342a9fe6c0560c68921595cb8b04e0c
dudley-codes <alexdudley@me.com>
2021-06-01 14:32:31 -0500

    Added sound effects

--- Program.cs
+++ Program.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 
 namespace Heist
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.Clear();
+      Console.Clear();
 
-            List<TeamMember> HeistTeam = new List<TeamMember>();
-            void header()
-            {
-                Console.WriteLine(@"
+      List<TeamMember> HeistTeam = new List<TeamMember>();
+      void header()
+      {
+        Console.WriteLine(@"
  /$$   /$$           /$$             /$$            /$$$$$$                                   
 | $$  | $$          |__/            | $$           /$$__  $$                                  
 | $$  | $$  /$$$$$$  /$$  /$$$$$$$ /$$$$$$        | $$  \__/  /$$$$$$  /$$$$$$/$$$$   /$$$$$$ 
@@ -23,100 +23,102 @@ namespace Heist
 |__/  |__/ \_______/|__/|_______/    \___/         \______/  \_______/|__/ |__/ |__/ \_______/           
             
                                                                                            ");
-            }
-            header();
-
-            Console.WriteLine("Plan Your Heist!");
-            Console.WriteLine("");
-            Console.Write("How many members are on your crew? ");
-            int teammembers;
-            while (true)
-            {
-                string members = Console.ReadLine();
-                if (int.TryParse(members, out teammembers))
-                {
-                    break;
-                }
-                Console.WriteLine("How about an actual number?");
-
-            }
-            Console.Clear();
-            for (int k = 0; k < teammembers; k++)
-            {
-
-                header();
-                Console.Write("Enter a name! ");
-                string Name = Console.ReadLine();
-                TeamMember Person = new TeamMember(Name);
-                Person.GetSkill();
-                Person.GetCourage();
-                HeistTeam.Add(Person);
-                Console.Clear();
-            }
-
-            // Console.WriteLine($"You have {HeistTeam.Count} members on your team");
-            header();
-            Console.Write("How many times are you going to try your crazy plan? ");
-            int runs = 0;
-            while (true)
-            {
-                string runstring = Console.ReadLine();
-                if (int.TryParse(runstring, out runs))
-                {
-                    break;
-                }
-                Console.WriteLine("How about an actual number?");
-
-            }
-            int DifficultyInt;
-            while (true)
-            {
-                Console.Write("How difficult would you like this heist to be? (0-100) ");
-                string DifficultyString = Console.ReadLine();
-                if (int.TryParse(DifficultyString, out DifficultyInt) && DifficultyInt > 0 && DifficultyInt < 100)
-                {
-                    break;
-                }
-                Console.WriteLine("How about a reasonable number?");
-            }
-
-
-            int Wins = 0;
-            int Losses = 0;
-
-            for (int j = 0; j < runs; j++)
-            {
-
-                int Difficulty = DifficultyInt;
-                int TeamSkillz = 0;
-
-                Random i = new Random();
-                int luck = i.Next(-10, 10);
-                Difficulty = Difficulty + luck;
-
-
-                Console.Clear();
-                header();
-                Console.Write($"Bank difficulty is {Difficulty}, ");
+      }
+      header();
 
 
 
-                foreach (TeamMember Person in HeistTeam)
-                {
-                    // Console.WriteLine($"{Person.Name}'s skill level is {Person.SkillLevel} and their courage level is {Person.Courage}!");
-                    TeamSkillz += Person.SkillLevel;
-                }
+      Console.WriteLine("Plan Your Heist!");
+      Console.WriteLine("");
+      Console.Write("How many members are on your crew? ");
+      int teammembers;
+      while (true)
+      {
+        string members = Console.ReadLine();
+        if (int.TryParse(members, out teammembers))
+        {
+          break;
+        }
+        Console.WriteLine("How about an actual number?");
 
-                Console.WriteLine($"Team Skillz {TeamSkillz}");
+      }
+      Console.Clear();
+      for (int k = 0; k < teammembers; k++)
+      {
+
+        header();
+        Console.Write("Enter a name! ");
+        string Name = Console.ReadLine();
+        TeamMember Person = new TeamMember(Name);
+        Person.GetSkill();
+        Person.GetCourage();
+        HeistTeam.Add(Person);
+        Console.Clear();
+      }
+
+      // Console.WriteLine($"You have {HeistTeam.Count} members on your team");
+      header();
+      Console.Write("How many times are you going to try your crazy plan? ");
+      int runs = 0;
+      while (true)
+      {
+        string runstring = Console.ReadLine();
+        if (int.TryParse(runstring, out runs))
+        {
+          break;
+        }
+        Console.WriteLine("How about an actual number?");
+
+      }
+      int DifficultyInt;
+      while (true)
+      {
+        Console.Write("How difficult would you like this heist to be? (0-100) ");
+        string DifficultyString = Console.ReadLine();
+        if (int.TryParse(DifficultyString, out DifficultyInt) && DifficultyInt > 0 && DifficultyInt < 100)
+        {
+          break;
+        }
+        Console.WriteLine("How about a reasonable number?");
+      }
+
+
+      int Wins = 0;
+      int Losses = 0;
+
+      for (int j = 0; j < runs; j++)
+      {
+
+        int Difficulty = DifficultyInt;
+        int TeamSkillz = 0;
+
+        Random i = new Random();
+        int luck = i.Next(-10, 10);
+        Difficulty = Difficulty + luck;
+
+
+        Console.Clear();
+        header();
+        Console.Write($"Bank difficulty is {Difficulty}, ");
+
+
+
+        foreach (TeamMember Person in HeistTeam)
+        {
+          // Console.WriteLine($"{Person.Name}'s skill level is {Person.SkillLevel} and their courage level is {Person.Courage}!");
+          TeamSkillz += Person.SkillLevel;
+        }
+
+        Console.WriteLine($"Team Skillz {TeamSkillz}");
 
 
 
 
-                if (Difficulty < TeamSkillz)
-                {
-                    Wins++;
-                    Console.WriteLine("You da best! You got all the monies");
-                    Console.WriteLine(@"
+        if (Difficulty < TeamSkillz)
+        {
+          Wins++;
+          Console.WriteLine("You da best! You got all the monies");
+          Console.WriteLine(@"
    ||====================================================================||
    ||//$\\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//$\\||
    ||(100)==================| FEDERAL RESERVE NOTE |================(100)||
@@ -141,12 +143,31 @@ namespace Heist
 ||\\$//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\$//||
 ||====================================================================||
 ");
-                    System.Threading.Thread.Sleep(4000);
-                }
-                else
-                {
-                    Losses++;
-                    Console.WriteLine($@"
+
+          void Beep(int freq, int time)
+          {
+            Console.Beep(freq, time);
+          }
+
+          void Sleep(int time)
+          {
+            System.Threading.Thread.Sleep(time);
+          }
+          // Intro 
+          Beep(330, 100); Sleep(100);
+          Beep(330, 100); Sleep(300);
+          Beep(330, 100); Sleep(300);
+          Beep(262, 100); Sleep(100);
+          Beep(330, 100); Sleep(300);
+          Beep(392, 100); Sleep(700);
+          Beep(196, 100); Sleep(700);
+
+          System.Threading.Thread.Sleep(2000);
+        }
+        else
+        {
+          Losses++;
+          Console.WriteLine($@"
   _________________________
      ||   ||     ||   ||
      ||   ||, , ,||   ||
@@ -166,14 +187,44 @@ namespace Heist
 |   go straight to jail!!   |
  '''''''''''''''''''''''''''
 ");
-                    Console.WriteLine("");
-                    System.Threading.Thread.Sleep(4000);
-                }
 
-            }
-            Console.Clear();
-            header();
-            Console.WriteLine($"Your team was successfull {Wins} times & failed miserably {Losses} times.");
+
+          void Beep(int freq, int time)
+          {
+            Console.Beep(freq, time);
+          }
+
+          void Sleep(int time)
+          {
+            System.Threading.Thread.Sleep(time);
+          }
+
+          Beep(370, 400); Sleep(400);
+          Beep(294, 400); Sleep(50);
+          Beep(247, 400); Sleep(300);
+          Beep(370, 400); Sleep(400);
+          Beep(294, 400); Sleep(400);
+          Beep(330, 400); Sleep(400);
+          Beep(370, 400); Sleep(50);
+          Beep(247, 400); Sleep(400);
+          Console.WriteLine("");
+          System.Threading.Thread.Sleep(2000);
         }
+
+      }
+      Console.Clear();
+      header();
+      Console.WriteLine($"Your team was successfull {Wins} times & failed miserably {Losses} times.");
     }
+
+    private static void Sleep(int v)
+    {
+      throw new NotImplementedException();
+    }
+
+    private static void Beep(int v1, int v2)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
